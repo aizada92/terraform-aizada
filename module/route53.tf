6b5a565d09751678180dafb48ea1bb071581fc32
aizada92 <aizadushka87@gmail.com>
2020-03-12 15:15:10 +0000

    update
    Signed-off-by: aizada92 <aizadushka87@gmail.com>

--- module/route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "wordpress" {
-  zone_id = "ZW7EPNG4WGZH9"
-  name    = "wordpress.cyber-pro.org"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_instance.web.public_ip}"]
-
-}
