a91986eca62d93d56a9d6f5bb58a7c418f9bd889
aizada92 <aizadushka87@gmail.com>
2020-03-12 14:44:20 +0000

    update
    Signed-off-by: aizada92 <aizadushka87@gmail.com>

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 backend "s3" {
 bucket = "terraform-class-aizada"
-key = "tower/us-east-1/tools/Sydney/tower.tfstate"
+key = "tower/us-east-1/tools/Seoul/tower.tfstate"
 region = "us-east-1"
   }
 }
