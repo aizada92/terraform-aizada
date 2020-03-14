f7264d210bf7ee5480551b7ce68327a7b462f131
aizada92 <aizadushka87@gmail.com>
2020-03-14 17:55:23 +0000

    add
    Signed-off-by: aizada92 <aizadushka87@gmail.com>

--- data_source/ec2.tf
+++ data_source/ec2.tf
@@ -1,3 +1,5 @@
+# Create a new instance of the latest Ubuntu 14.04 on an
+# t2.micro node with an AWS Tag naming it "HelloWorld"
 provider "aws" {
   region = "us-west-2"
 }
@@ -12,13 +14,13 @@ data "aws_ami" "ubuntu" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["099720109477"] # Canonical
+  owners = ["099720109477"] # Canonical #This owner never changes
 }
 output "UBUNTU_AMI_ID" {
-    value = "${data.aws_ami.ubuntu.id}"
+  value = "${data.aws_ami.ubuntu.id}"
 }
-#Gets Centos AMI
-data "aws_ami" "centos" {
+#Gets  CentOS AMI
+data "aws_ami" "centos" { #should be uniq
   most_recent = true
   filter {
     name   = "name"
@@ -28,10 +30,10 @@ data "aws_ami" "centos" {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-  owners = ["679593333241"] # Canonical
+  owners = ["679593333241"] # Canonical #This owner never changes
 }
 output "CENTOS_AMI_ID" {
-    value = "${data.aws_ami.centos.id}"
+  value = "${data.aws_ami.centos.id}"
 }
 resource "aws_key_pair" "provisioner" {
   key_name   = "provisioner-key"
@@ -41,29 +43,24 @@ resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
   key_name = "${aws_key_pair.provisioner.key_name}"
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "ubuntu"
-      private_key = "${file("~/.ssh/id_rsa")}"
-      host     = "${self.public_ip}"
+# Copies the file as the root user using SSH
+provisioner "remote-exec" {
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = "${file("~/.ssh/id_rsa")}"
+    host     = "${self.public_ip}"
     }
-       
-       
-        inline = [
-            "sudo atp-get install telnet -y",
-            "sudo mkdir /tmp/ubuntu",
-            "w",
-            "free -m",
-            "sleep 5"
-        ]
-    }
+     inline= [
+       " sudo apt-get install telnet -y",
+       "sudo mkdir /tmp/ubuntu", 
+       "w",
+       "free -m",
+       "sleep 5"
+       ]
+  }
+   
   tags = {
     Name = "HelloWorld"
   }
 }
-
-
-
-
-
--- data_source/null_resource.tf
+++ data_source/null_resource.tf
@@ -4,31 +4,23 @@ resource "null_resource" "aizada" {
     }
     provisioner "remote-exec" {
     connection {
-      type     = "ssh"
-      user     = "centos"
-      private_key = "${file("~/.ssh/id_rsa")}"
-      host     = "${aws_instance.web.public_ip}"
+        type     = "ssh"
+        user     = "ubuntu"
+        private_key = "${file("~/.ssh/id_rsa")}"
+        host     = "${aws_instance.web.public_ip}"
+
     }
-       
-    inline = [
-        "sudo apt-get install telnet -y",
-        "sudo mkdir /tmp/ubuntu",
-        "w",
-        "sleep 5",
-        "sudo apt-get install wget -y",
-        "wget https://wordpress.org/latest.tar.gz"
-        ]
-    } 
+
+        inline = [
+          "sudo apt-get install telnet -y",
+          "sudo mkdir /tmp/ubuntu",
+          "w",
+          "free-m",
+          "sleep 5",
+          "sudo apt-get install wget -y",
+          "wget https://wordpress.org/latest.tar.gz"
+
+       ]
+    }
+  
 }
-
-
-
-
-
-
-
-        
-   
-   
-
-
