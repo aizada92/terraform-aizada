284049a06aebc479523ae18c30b4ab4bd079ba88
aizada92 <aizadushka87@gmail.com>
2020-03-16 22:41:29 +0000

    update
    Signed-off-by: aizada92 <aizadushka87@gmail.com>

--- infrastructure/awx/aws/terraform.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  required_version = "0.11.14"
-  required_providers {
-    aws = "~> 2.49"  
-  }
-}
--- infrastructure/example/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-backend "s3" {
-bucket = "acirrustech-iaac"
-key = "r1soft/us-east-1/tools/tools/jenkins.tfstate"
-region = "us-east-1"
-  }
-}
