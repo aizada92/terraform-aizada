b9b02e4e289feaee6a86c948b4833c8e74018a01
aizada92 <aizadushka87@gmail.com>
2020-03-16 19:32:21 +0000

    add
    Signed-off-by: aizada92 <aizadushka87@gmail.com>

--- infrastructure/r1soft/module/sec_group.tf
+++ infrastructure/r1soft/module/sec_group.tf
@@ -19,5 +19,5 @@ resource "aws_security_group_rule" "allow_all_ingress" {
   to_port         = 65535
   protocol        = "tcp"
   cidr_blocks     = ["0.0.0.0/0"]                            
-  security_group_id = "${aws_security_group.allow_ssh_and_r1soft.id}"
+  security_group_id = ["${aws_security_group.allow_ssh_and_r1soft.id}"]
 }
